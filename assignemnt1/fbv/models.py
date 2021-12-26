6b5fe2797ac70b6c24aa3c4ef8515a1ec8dfbd2d
hectoradrian961030 <hectoradrian961030@gmail.com>
2021-12-26 13:35:04 -0500

    issue

--- assignemnt1/fbv/models.py
+++ assignemnt1/fbv/models.py
@@ -6,7 +6,7 @@ class Passenger(models.Model):
 
     firstName = models.CharField(max_length=128)
     lastName = models.CharField(max_length=128)
-    travelPoints = models.CharField(max_length=256)
+    travelPoints = models.DecimalField(max_digits=10, decimal_places=3)
 
     def __str__(self):
         return self.firstName + " " + self.lastName
